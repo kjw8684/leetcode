1e361268ecc960e337b28eca8d364781f26726ee
Joo-ProtoType <90236643+Joo-ProtoType@users.noreply.github.com>
2023-08-04 17:33:36 +0900

    Time: 1 ms (11.3%), Space: 45.1 MB (42.43%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -2,7 +2,7 @@ class Solution {
     public int search(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == target){
-                return i;
+                return i; 
             }
         }
         return -1;
