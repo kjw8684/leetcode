d5348139c8338a6c5d056812bd94841678e82a3a
Joo-ProtoType <90236643+Joo-ProtoType@users.noreply.github.com>
2023-09-08 07:20:00 +0900

    Time: 6 ms (6.06%), Space: 40 MB (74.35%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+        //head 값이 없을경우와 바뀌는 범위가 1일경우 원값과 같음으로 바로 head를 return한다.
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        // dummy, prev = [0,head]
+        for (int i = 0; i < left - 1; ++i) {
+            prev = prev.next;
+        }
+        //prev 값을 left 시작값으로 설정해둠
+        ListNode current = prev.next;
+        //current 값을 left 위치부터 남은값 전부로 설정
+        for (int i = 0; i < right - left; ++i) {
+            ListNode nextNode = current.next;
+            current.next = nextNode.next;
+            nextNode.next = prev.next;
+            prev.next = nextNode;
+        }
+        while(prev != null){
+            System.out.println(prev.val);
+            prev = prev.next;
+        }
+        return dummy.next;
+    }
+}
