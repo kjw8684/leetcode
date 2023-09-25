0fa59f8e94bc151550d2bcba2a04eb808ced19ca
Joo-ProtoType <90236643+Joo-ProtoType@users.noreply.github.com>
2023-09-25 20:29:55 +0900

    Time: 31 ms (5.98%), Space: 44.3 MB (6.27%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,24 +1,10 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character, Integer> map_s = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            boolean is_null = map_s.get( s.charAt(i) ) == null;
-            if (is_null) {
-                map_s.put (s.charAt(i), 0);
-            }
-            map_s.put (s.charAt(i), map_s.get( s.charAt(i) ) + 1);
+            t = t.replaceFirst(Character.toString(s.charAt(i)), "");
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            boolean found_difference = map_s.get( t.charAt(i) ) == null || map_s.get( t.charAt(i) ) == 0;
-            if (found_difference) {
-                return t.charAt(i);
-            }else {
-                map_s.put (t.charAt(i), map_s.get( t.charAt(i) ) - 1);
-            }
-        }
-
-        return 'x';
+        return t.charAt(0);
     }
 }
