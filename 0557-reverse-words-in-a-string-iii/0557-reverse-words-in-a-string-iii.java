b9430c7d8694f6c7655d3b0b8e71fd535332100d
Joo-ProtoType <90236643+Joo-ProtoType@users.noreply.github.com>
2023-10-01 18:12:09 +0900

    Time: 27 ms (17.75%), Space: 44.7 MB (16.17%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,21 +1,18 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<Character> stack = new Stack<>();
+        String temp = "";
         StringBuilder answer = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            boolean is_blank = s.charAt(i) != ' ';
-            if (is_blank) {
-                stack.push(s.charAt(i)); 
+            boolean is_not_blank = s.charAt(i) != ' ';
+            if (is_not_blank) {
+                temp = s.charAt(i) + temp;
             }else {
-                while (!stack.empty()) {
-                    answer.append(stack.pop());
-                }
+                answer.append(temp);
                 answer.append(" ");
+                temp = "";
             }
         }
-        while (!stack.empty()) {
-            answer.append(stack.pop());
-        }
+        answer.append(temp);
         return answer.toString();
     }
 }
