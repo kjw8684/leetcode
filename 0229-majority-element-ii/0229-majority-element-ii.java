6dd70fc7ac66eb52e1d90b4f46e7000ba9f641bc
Joo-ProtoType <90236643+Joo-ProtoType@users.noreply.github.com>
2023-10-05 09:19:54 +0900

    Time: 5 ms (52.84%), Space: 47.4 MB (5.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,29 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int condition = nums.length / 3;
-        HashMap<Integer, Integer> num = new HashMap<>();
+        Arrays.sort(nums);
+        int condition = nums.length / 3, cur_num_count = 0;
         List<Integer> answer = new ArrayList<>();
-        for (int i : nums) {
-            if (num.get(i) == null) {
-                num.put(i, 1);
-            }else {
-                num.put(i, num.get(i) + 1);
-            }
 
-            if (num.get(i) > condition && !answer.contains(i)) {
-                answer.add(i);
+        if (condition < 1) {
+            for (int i : nums) {
+                if (!answer.contains(i)) {
+                    answer.add(i);
+                }
+            }
+        }else {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i - 1] == nums[i]) {
+                    cur_num_count++;
+                }else {
+                    cur_num_count = 0;
+                }
+
+                if (cur_num_count == condition && !answer.contains(i)) {
+                    answer.add(nums[i]);
+                }
             }
         }
+
         return answer;
     }
 }
