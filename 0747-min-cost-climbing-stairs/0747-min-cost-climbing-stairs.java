01b0e77cf1d2070f2a9fbf057b8cf5b1d7039eb9
Joo-ProtoType <90236643+Joo-ProtoType@users.noreply.github.com>
2023-10-13 10:25:04 +0900

    Time: 1 ms (46.22%), Space: 42.6 MB (93.17%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,13 +1,11 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int cl = cost.length;
+        int cl = cost.length, pay1_min, pay2_min;
         int[] pay1 = new int[cl], pay2 = new int[cl];
         pay1[0] = cost[0];
         pay1[1] = cost[1] + cost[0];
-
         pay2[0] = cost[1];
         pay2[1] = cost[1];
-
 
         for (int i = 2; i < cl; i++) {
             if (pay1[i - 1] < pay1[i - 2]) {
@@ -16,7 +14,7 @@ class Solution {
                 pay1[i] = pay1[i - 2] + cost[i];
             }
         }
-        int pay1_min = pay1[cl - 1] < pay1[cl - 2] ? pay1[cl - 1] : pay1[cl - 2];
+        pay1_min = pay1[cl - 1] < pay1[cl - 2] ? pay1[cl - 1] : pay1[cl - 2];
 
         for (int i = 2; i < cl; i++) {
             if (pay2[i - 1] < pay2[i - 2]) {
@@ -25,7 +23,7 @@ class Solution {
                 pay2[i] = pay2[i - 2] + cost[i];
             }
         }
-        int pay2_min = pay2[cl - 1] < pay2[cl - 2] ? pay2[cl - 1] : pay2[cl - 2];
+        pay2_min = pay2[cl - 1] < pay2[cl - 2] ? pay2[cl - 1] : pay2[cl - 2];
 
         return pay1_min < pay2_min ? pay1_min : pay2_min;
     }
