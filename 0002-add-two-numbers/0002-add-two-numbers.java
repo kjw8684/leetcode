60bcbd4c9c8d7a5e2231f13fb31dc9d7cef332c1
Joo-ProtoType <90236643+Joo-ProtoType@users.noreply.github.com>
2023-10-14 13:28:02 +0900

    Time: 1 ms (100%), Space: 43.7 MB (15.14%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,35 +10,35 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0), cur = dummy;
-        int temp, pass = 0;
+        ListNode answer = new ListNode(0), cur = answer;
+        int val, pass = 0;
 
         while (l1 != null || l2 != null) {
-            temp = pass;
+            val = pass;
             pass = 0;
 
             if (l1 != null) {
-                temp += l1.val;
+                val += l1.val;
                 l1 = l1.next;
             }
             if (l2 != null) {
-                temp += l2.val;
+                val += l2.val;
                 l2 = l2.next;
             }
 
-            if (temp > 9) {
+            if (val > 9) {
                 pass = 1;
-                temp -= 10;
+                val -= 10;
             }
-            
-            cur.next = new ListNode(temp);
+
+            cur.next = new ListNode(val);
             cur = cur.next;
         }
         if (pass == 1) {
             cur.next = new ListNode(1);
         }
 
-        return dummy.next;
+        return answer.next;
     }
 
 }
