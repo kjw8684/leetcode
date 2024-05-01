550aef64cee4ec6a4cafe11d2c6041288e54607a
kim_joowon <kjw8684@naver.com>
2024-05-01 09:16:52 +0900

    Time: 0 ms (100.00%), Space: 41.4 MB (81.91%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,11 +1,10 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
         StringBuilder sb = new StringBuilder();
-        Stack<Character> stack = new Stack<>();
         int len = word.length(), index = 0;
         
         for(int i = 0; i < len; i++) {
-            stack.push(word.charAt(i));
+            sb.insert(0, word.charAt(i));
             if(word.charAt(i) == ch) {
                 index = i;
                 break;
@@ -15,11 +14,7 @@ class Solution {
         if(index == 0) {
             return word;
         }
-        
-        while(!stack.isEmpty()) {
-            sb.append(stack.pop());
-        }
-        
+
         sb.append(word.substring(index + 1, len));
         return sb.toString();
     }
