d88b30b4fd5ecf85a1c1854410322cdf9bae7c23
kim_joowon <kjw8684@naver.com>
2024-06-14 09:05:16 +0900

    Time: 38 ms (43.61%), Space: 59.4 MB (86.89%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -4,9 +4,9 @@ class Solution {
         Arrays.sort(nums);
         
         for(int i = 1; i < len; i++) {
-            while(nums[i - 1] >= nums[i]) {
-                answer++;
-                nums[i]++;
+            if(nums[i - 1] >= nums[i]) {
+                answer += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
             }
         }
         
