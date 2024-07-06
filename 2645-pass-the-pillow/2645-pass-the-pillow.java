f4535fa126cc932764b2c3d1608636985bafe60d
kim_joowon <kjw8684@naver.com>
2024-07-06 16:20:18 +0900

    Time: 0 ms (100%), Space: 40.1 MB (74.29%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -1,10 +1,8 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int way = time / (n - 1), position = time % (n - 1);
-
-        if(way % 2 == 0) {
-            return position + 1;
+        if((time / (n - 1)) % 2 == 0) {
+            return (time % (n - 1)) + 1;
         }
-        return n - position;
+        return n - (time % (n - 1));
     }
 }
