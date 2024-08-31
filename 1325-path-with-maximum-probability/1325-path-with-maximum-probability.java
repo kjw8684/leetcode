0e0df258e1e0b8617da6e86ece8730d544e2b61f
kim_joowon <kjw8684@naver.com>
2024-08-31 12:15:14 +0900

    Time: 11 ms (60.39%), Space: 56.7 MB (49.93%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,27 +1,27 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        double[] maxProb = new double[n];
-        maxProb[start_node] = 1.0;
+        double[] map = new double[n];
+        int len = edges.length;
+        boolean cheak = false;
+        map[start_node] = 1.0;
 
-        for (int i = 0; i < n - 1; i++) {
-            boolean updated = false;
-            for (int j = 0; j < edges.length; j++) {
-                int u = edges[j][0];
-                int v = edges[j][1];
-                double prob = succProb[j];
-
-                if (maxProb[u] * prob > maxProb[v]) {
-                    maxProb[v] = maxProb[u] * prob;
-                    updated = true;
+        for(int i = 0; i < n; i++) {
+            cheak = false;
+            for(int j = 0; j < len; j++) {
+                if(map[edges[j][0]] < map[edges[j][1]] * succProb[j]) {
+                    map[edges[j][0]] = map[edges[j][1]] * succProb[j];
+                    cheak = true;
                 }
-                if (maxProb[v] * prob > maxProb[u]) {
-                    maxProb[u] = maxProb[v] * prob;
-                    updated = true;
+                if(map[edges[j][1]] < map[edges[j][0]] * succProb[j]) {
+                    map[edges[j][1]] = map[edges[j][0]] * succProb[j];
+                    cheak = true;
                 }
             }
-            if (!updated) break;
+            if(!cheak) {
+                break;
+            }
         }
 
-        return maxProb[end_node];
+        return map[end_node];
     }
 }
