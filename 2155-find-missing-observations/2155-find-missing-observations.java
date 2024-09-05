b6cec691984ce80d21e82c3a316f1fc3127c4cb0
kim_joowon <kjw8684@naver.com>
2024-09-05 10:55:57 +0900

    Time: 12 ms (7.87%), Space: 59.9 MB (71.48%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.java
+++ 2155-find-missing-observations/2155-find-missing-observations.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
-        int len = rolls.length, left = mean * (n + len) - Arrays.stream(rolls).sum(), cur = 0;
+        int len = rolls.length, left = mean * (n + len) - Arrays.stream(rolls).sum();
         int[] answer = new int[n];
         if(left > n * 6 || left < n) {
             return new int[]{};
