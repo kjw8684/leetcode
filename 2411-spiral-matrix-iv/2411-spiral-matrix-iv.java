28ced91e5b9e52580d71efbd2f56f2c1ec99fe2f
kim_joowon <kjw8684@naver.com>
2024-09-09 12:03:16 +0900

    Time: 7 ms (87.82%), Space: 56.6 MB (81.69%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.java
@@ -10,10 +10,7 @@
  */
 class Solution {
     public int[][] spiralMatrix(int m, int n, ListNode head) {
-        if(head == null) {
-            return new int[0][0];
-        }
-        int[] cur = new int[2];
+        int[] cur = new int[] {0, -1};
         int[][] answer = new int[m][n];
 
         for(int i = 0; i < m; i++) {
@@ -23,44 +20,29 @@ class Solution {
         }
 
         m--;
-        cur[1] = -1;
         while(head != null) {
-            for(int i = 0; i < n; i++) {
-                if(head == null) {
-                    break;
-                }
+            for(int i = 0; i < n && head != null; i++) {
                 cur[1]++;
                 answer[cur[0]][cur[1]] = head.val;
                 head = head.next;
-                
             }
             n--;
 
-            for(int i = 0; i < m; i++) {
-                if(head == null) {
-                    break;
-                }
+            for(int i = 0; i < m && head != null; i++) {
                 cur[0]++;
-                System.out.println(cur[0] + " " + cur[1]);
                 answer[cur[0]][cur[1]] = head.val;
                 head = head.next;
             }
             m--;
 
-            for(int i = 0; i < n; i++) {
-                if(head == null) {
-                    break;
-                }
+            for(int i = 0; i < n && head != null; i++) {
                 cur[1]--;
                 answer[cur[0]][cur[1]] = head.val;
                 head = head.next;
             }
             n--;
 
-            for(int i = 0; i < m; i++) {
-                if(head == null) {
-                    break;
-                }
+            for(int i = 0; i < m && head != null; i++) {
                 cur[0]--;
                 answer[cur[0]][cur[1]] = head.val;
                 head = head.next;
