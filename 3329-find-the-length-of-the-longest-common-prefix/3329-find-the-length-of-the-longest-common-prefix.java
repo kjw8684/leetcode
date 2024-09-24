05cc0ea8cc1a59608e6042b6c63bf875ce0514e8
kim_joowon <kjw8684@naver.com>
2024-09-24 13:17:02 +0900

    Time: 93 ms (45.71%), Space: 56.1 MB (43.26%) - LeetHub

---
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        Set<Integer> set = new HashSet<>();
+        int max = 0, len1 = arr1.length, len2 = arr2.length, temp1 = 0, temp2 = 0;
+
+        for(int i = 0; i < len1; i++) {
+            while(arr1[i] != 0) {
+                set.add(arr1[i]);
+                arr1[i] /= 10;
+            }
+        }
+
+        for(int i = 0; i < len2; i++) {
+            while((int) Math.log10(arr2[i]) + 1 > max) {
+                if(set.contains(arr2[i])) {
+                    max = (int) Math.log10(arr2[i]) + 1;
+                    break;
+                }
+                arr2[i] /= 10;
+            }
+        }
+
+        return max;
+    }
+}
