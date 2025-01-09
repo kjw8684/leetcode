58332becf71155b5e409e5477831ba38ff176937
Kim Joo won <kjw8684@naver.com>
2025-01-09 12:20:43 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
@@ -3,7 +3,7 @@ class Solution {
         int len = words.length, slen = pref.length(), count = 0;
 
         for(int i = 0; i < len; i++) {
-            if(slen <= words[i].length() && words[i].substring(0, slen).equals(pref)) {
+            if(words[i].startWith(pref)) {
                 count++;
             }
         }
