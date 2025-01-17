63b03a4aff6617f1aa61faaad91caca931d1ad4d
Kim Joo won <kjw8684@naver.com>
2025-01-17 21:44:11 +0900

    Time: 9 ms (18.24%), Space: 62.3 MB (49.41%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int len = derived.length, original = 0;
+        int len = derived.length;
+        boolean answer = true;
 
         for(int i = 0; i < len; i++) {
             if(derived[i] == 1) {
-                original = Math.abs(original - 1);
+                answer = !answer;
             }
         }
 
-        return original == 0;
+        return answer;
     }
 }
