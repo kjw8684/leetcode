d86e6544e83f529749932afe5ea992ea1a68c769
Kim Joo won <kjw8684@naver.com>
2025-02-18 15:07:59 +0900

    Time: 1 ms (75.25%), Space: 42.1 MB (18.52%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -1,16 +1,18 @@
 class Solution {
     public String smallestNumber(String pattern) {
         int len = pattern.length();
-        int[] stack = new int[len + 1];
+        Stack<Integer> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
         int index = 0;
 
         for(int i = 0; i <= len; i++) {
-            stack[index++] = i + 1;
+            stack.push(i + 1);
+            index++;
 
             if(i == len || pattern.charAt(i) == 'I') {
                 while(index > 0) {
-                    sb.append(stack[--index]);
+                    index--;
+                    sb.append(stack.pop());
                 }
             } 
         }
