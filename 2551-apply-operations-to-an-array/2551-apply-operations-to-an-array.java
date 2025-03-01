877329f618ae35060826daaefcd751475e943500
Kim Joo won <kjw8684@naver.com>
2025-03-01 10:51:25 +0900

    Time: 2 ms (8.49%), Space: 44.9 MB (5.12%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,23 +1,35 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
         int len = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        if(nums[0] != 0) {
+            stack.add(nums[0]);
+        }
 
         for(int i = 1; i < len; i++) {
-            if(nums[i - 1] == nums[i]) {
+            if(nums[i] == 0) {
+                continue;
+            }
+            else if(nums[i - 1] == nums[i]) {
+                stack.pop();
+                stack.add(nums[i] * 2);
                 nums[i - 1] = nums[i - 1] * 2;
                 nums[i] = 0;
             }
-        }
-
-        for(int i = 0; i < len; i++) {
-            for(int j = i + 1; j < len; j++) {
-                if(nums[i] == 0 && nums[j] != 0) {
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                }
+            else {
+                stack.add(nums[i]);
             }
         }
 
-        return nums;
+        int[] answer = new int[len];
+        int index = 0;
+        Collections.reverse(stack);
+
+        while(!stack.isEmpty()) {
+            answer[index] = stack.pop();
+            index++;
+        }
+
+        return answer;
     }
 }
