4a9818ab2e20d9288e3765fc40bb751754aa1e32
Kim Joo won <kjw8684@naver.com>
2025-03-19 11:57:25 +0900

    Time: 8 ms (34.91%), Space: 57.9 MB (13.14%) - LeetHub

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int len = nums.length, count = 0;
+
+        for(int i = 2; i < len; i++) {
+            if(nums[i - 2] == 0) {
+                count++;
+                flip(nums, i);
+            }
+        }
+
+        if(nums[len - 2] == 1 && nums[len - 1] == 1) {
+            return count;
+        }
+        return -1;
+    }
+
+    public void flip(int[] nums, int index) {
+        for(int i = 2; i >= 0; i--) {
+            nums[index - i] = Math.abs(nums[index - i] - 1);
+        }
+    }
+}
