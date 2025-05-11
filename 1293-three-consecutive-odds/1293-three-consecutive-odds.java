7db3501154665288d17919b14f35ad704a5323c7
Kim Joo won <kjw8684@naver.com>
2025-05-11 11:29:53 +0900

    Time: 0 ms (100%), Space: 42.8 MB (13.32%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.java
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int count = 0, len = arr.length;
-        
-        for(int i = 0; i < len; i++) {
-            if(arr[i] % 2 == 1) {
+        int count = 0;
+        for(int num : arr) {
+            if(num % 2 == 1) {
                 count++;
             }
             else {
                 count = 0;
             }
-            
+
             if(count == 3) {
                 return true;
             }
         }
-        
+
         return false;
     }
 }
