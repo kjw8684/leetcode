c6aabf91f04301d2fb45150982278236a243b9ab
Kim Joo won <kjw8684@naver.com>
2025-07-01 22:40:35 +0900

    Time: 0 ms (100%), Space: 42.3 MB (38.84%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,13 +1,14 @@
 class Solution {
     public int possibleStringCount(String word) {
         int answer = 1, len = word.length();
-        char before = word.charAt(0);
+        char before = word.charAt(0), current = '1';
 
         for(int i = 1; i < len; i++) {
-            if(before == word.charAt(i)) {
+            current = word.charAt(i);
+            if(before == current) {
                 answer++;
             }
-            before = word.charAt(i);
+            before = current;
         }
 
         return answer;
