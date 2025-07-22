69a2b9fc12c4fe81442e8b78f18f56db9f61509a
Kim Joo won <kjw8684@naver.com>
2025-07-22 10:42:02 +0900

    Time: 56 ms (30.22%), Space: 57.5 MB (84.62%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,33 +1,28 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         int len = nums.length, leftPointer = 0, rightPointer = 0, max = 0;
-        int[] dp = new int[len];
-        dp[0] = nums[0];
-
-        for(int i = 1; i < len; i++) {
-            dp[i] += nums[i] + dp[i - 1];
-        }
 
         Set<Integer> set = new HashSet<>();
-        int left = 0;
+        int left = 0, right = 0;
         while(rightPointer < len) {
             while(!set.contains(nums[rightPointer])) {
                 set.add(nums[rightPointer]);
+                right += nums[rightPointer];
                 rightPointer++;
                 if(rightPointer == len) {
                     break;
                 }
             }
-            max = Math.max(max, dp[rightPointer - 1] - left);
+            max = Math.max(max, right - left);
             if(rightPointer == len) {
                 break;
             }
 
             while(set.contains(nums[rightPointer])) {
                 set.remove(nums[leftPointer]);
+                left += nums[leftPointer];
                 leftPointer++;
             }
-            left = dp[leftPointer - 1];
         }
 
         return max;
