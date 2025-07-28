d95536772103629b5c65ba04c10d595ffdbdce7d
Kim Joo won <kjw8684@naver.com>
2025-07-28 17:51:51 +0900

    Time: 6 ms (95.95%), Space: 41.3 MB (46.7%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,16 +1,18 @@
 class Solution {
+    int len = 0;
     public void backtrack(int[] nums, int index, int currentOR, int maxOR, int[] count) {
         if (currentOR == maxOR) {
             count[0]++;
         }
 
-        for (int i = index; i < nums.length; i++) {
+        for (int i = index; i < len; i++) {
             backtrack(nums, i + 1, currentOR | nums[i], maxOR, count);
         }
     }
 
     public int countMaxOrSubsets(int[] nums) {
         int max = 0;
+        len = nums.length;
 
         for (int num : nums) {
             max |= num;
