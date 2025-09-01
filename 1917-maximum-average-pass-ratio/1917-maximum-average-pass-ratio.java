8d65891794443cd52823fc98d0bce6bd8ae8d706
Kim Joo won <kjw8684@naver.com>
2025-09-01 11:52:59 +0900

    Time: 499 ms (19.17%), Space: 96.2 MB (38.34%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -1,54 +1,28 @@
 class Solution {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<ClassRecord> pq = new PriorityQueue<>(new Compare());
-        
-        for(int[] cl : classes)
-            pq.add(new ClassRecord(cl));
-        
-        ClassRecord cl;
-        while(extraStudents-- > 0)
-            pq.add(pq.remove().addOneStudent());
-        
-        double sum = 0;
-        while(!pq.isEmpty()){
-            cl = pq.remove();
-            sum += (double)cl.pass / cl.total;
+        PriorityQueue<double[]> pq = new PriorityQueue<>(
+            (a, b) -> Double.compare(
+                (b[0] + 1) / (b[1] + 1) - b[0] / b[1],
+                (a[0] + 1) / (a[1] + 1) - a[0] / a[1]
+            )
+        );
+
+        for (int[] c : classes) {
+            pq.add(new double[]{c[0], c[1]});
         }
 
-        return sum / classes.length;
-    }
-}
+        while (extraStudents-- > 0) {
+            double[] cur = pq.poll();
+            cur[0]++;
+            cur[1]++;
+            pq.add(cur);
+        }
 
-class ClassRecord{
-    int pass;
-    int total;
-    double inc;
-
-    public ClassRecord(int[] array){
-        pass = array[0];
-        total = array[1];
-        inc = getIncrement();
-    }
-
-    public ClassRecord addOneStudent(){
-        pass++;
-        total++;
-        inc = getIncrement();
-        return this;
-    }
-
-    private double getIncrement(){
-        return (pass + 1.0) / (total + 1) - (double)pass / total;
-    }
-}
-
-class Compare implements Comparator<ClassRecord>{
-    public int compare(ClassRecord a, ClassRecord b){
-        if(a.inc < b.inc)
-            return 1;
-        else if(a.inc > b.inc)
-            return -1;
-        else
-            return 0;
+        double answer = 0;
+        while (!pq.isEmpty()) {
+            double[] cur = pq.poll();
+            answer += cur[0] / cur[1];
+        }
+        return answer / classes.length;
     }
 }
