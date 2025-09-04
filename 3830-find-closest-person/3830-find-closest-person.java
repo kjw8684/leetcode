6886207ce8a80e7e54df5bb295bf84c757e6bc06
Kim Joo won <kjw8684@naver.com>
2025-09-04 09:04:09 +0900

    Time: 0 ms (100%), Space: 41 MB (41.65%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.java
+++ 3830-find-closest-person/3830-find-closest-person.java
@@ -2,12 +2,9 @@ class Solution {
     public int findClosest(int x, int y, int z) {
         int difx = Math.abs(x - z), dify = Math.abs(y - z);
 
-        if(difx < dify) {
-            return 1;
+        if(difx == dify) {
+            return 0;
         }
-        else if(difx > dify) {
-            return 2;
-        }
-        return 0;
+        return difx > dify ? 2 : 1;
     }
 }
