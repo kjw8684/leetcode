b970738aa38ee211318c779baae6109887de815f
Kim Joo won <kjw8684@naver.com>
2025-09-28 20:43:31 +0900

    Time: 1997 ms (5.04%), Space: 45.6 MB (10.43%) - LeetHub

---
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        int len = nums.length;
+        Arrays.sort(nums);
+
+        for(int i = len - 1; i >= 0; i--) {
+            for(int j = i - 1; j >= 0; j--) {
+                for(int k = j - 1; k >= 0; k--) {
+                    if(nums[k] + nums[j] > nums[i]) {
+                        return nums[i] + nums[j] + nums[k];
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
