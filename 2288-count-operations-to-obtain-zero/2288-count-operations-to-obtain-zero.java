3d5354a555b3ed97438ce54230b93b4989ae6c15
Kim Joo won <kjw8684@naver.com>
2025-11-09 11:00:28 +0900

    Time: 1 ms (90.35%), Space: 42.3 MB (14.07%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
@@ -1,6 +1,5 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-        if(num1 == 0 || num2 == 0) return 0;
         int count = 0;
 
         while(num1 != 0 && num2 != 0) {
