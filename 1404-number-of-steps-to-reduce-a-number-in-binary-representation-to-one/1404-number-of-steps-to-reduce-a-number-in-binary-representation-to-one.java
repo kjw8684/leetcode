c5e3e28fab8ec96aa5b1dc51f41d1aa0f5d568d0
Kim Joo won <kjw8684@naver.com>
2026-02-26 16:56:59 +0900

    Time: 1 ms (49.85%), Space: 43 MB (31.27%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,21 +1,37 @@
-import java.math.BigInteger;
-
 class Solution {
     public int numSteps(String s) {
-        int count = 0;
-        BigInteger current = new BigInteger(s, 2), two = new BigInteger("2");
-        
-        while(!current.equals(BigInteger.ONE)) {
-            if(current.remainder(two) == BigInteger.ZERO) {
-                current = current.divide(two);
+        int len = s.length(), count = 0, one = 0;
+        boolean over = false;
+
+        for(int i = 1; i < len; i++) {
+            if(s.charAt(i) == '1') {
+                one++;
+            }
+        }
+        if(one == 0) {
+            return len - 1;
+        }
+
+
+        for(int i = len - 1; i >= 0; i--) {
+            char cur = s.charAt(i);
+            if(over && cur == '1') {
+                over = true;
+            }
+            else if(over && cur == '0') {
+                count++;
+                over = true;
+            }
+            else if(!over && cur == '1') {
+                count++;
+                over = true;
             }
             else {
-                current = current.add(BigInteger.ONE);
+                over = false;
             }
-            
             count++;
         }
-        
+
         return count;
     }
 }
