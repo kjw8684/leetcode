5c855a0d19d1e610a020eda59ee7a97c87c04fd1
Kim Joo won <kjw8684@naver.com>
2026-05-04 14:29:22 +0900

    Time: 0 ms (100%), Space: 43.7 MB (69.68%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+
+        if(len % 2 == 1) {
+            for(int i = 0; i < len / 2; i++) {
+                for(int j = 0; j < (len / 2) + 1; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[len - j - 1][i];
+                    matrix[len - j - 1][i] = matrix[len - i - 1][len - j - 1];
+                    matrix[len - i - 1][len - j - 1] = matrix[j][len - i - 1];
+                    matrix[j][len - i - 1] = temp;
+                }
+            }
+        }
+        else {
+            for(int i = 0; i < len / 2; i++) {
+                for(int j = 0; j < len / 2; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[len - j - 1][i];
+                    matrix[len - j - 1][i] = matrix[len - i - 1][len - j - 1];
+                    matrix[len - i - 1][len - j - 1] = matrix[j][len - i - 1];
+                    matrix[j][len - i - 1] = temp;
+                }
+            }
+        }
+
+    }
+}
