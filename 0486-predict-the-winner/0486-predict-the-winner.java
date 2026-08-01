935bf632f5eb1e4905949d64b18e875e5070f9dc
Kim Joo won <kjw8684@naver.com>
2026-08-01 10:55:42 +0900

    Time: 0 ms (100%), Space: 42.5 MB (89.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,14 +1,23 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        int n = nums.length;
-        if (n % 2 == 0) return true;
-        
-        int[] dp = nums.clone();
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+        int len = nums.length;
+        int[][] dp = new int[len][len];
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int length = 2; length <= len; length++) {
+            for (int l = 0; l <= len - length; l++) {
+                int r = l + length - 1;
+
+                dp[l][r] = Math.max(
+                    nums[l] - dp[l + 1][r],
+                    nums[r] - dp[l][r - 1]
+                );
             }
         }
-        return dp[n - 1] >= 0;
+
+        return dp[0][len - 1] >= 0;
     }
 }
